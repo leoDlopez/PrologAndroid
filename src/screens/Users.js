61c5eef600b25728167a22f4e862da085e4b5c63
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-06-27 02:53:28 -0500

    Update Users.js

--- src/screens/Users.js
+++ src/screens/Users.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component, useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const styles = StyleSheet.create({
     body: {
@@ -11,13 +11,36 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         margin: 10
-    }
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        width: 200
+    },
 })
 
 function Users() {
+    const [values, setValues] = useState({
+        name: '',
+    })
+
+    function handleChangeName(value) {
+        setValues(values => ({
+            ...values,
+            name: value,
+        }))
+    }
+
     return (
         <View style={styles.body}>
             <Text style={styles.text}>Esta es la pantalla de usuarios xd</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={text => handleChangeName(text)}
+                value={values.name}
+                placeholder="Introduce tu nombre"
+            />
         </View>
     )
 }
